b005c1970ac9fe476c7768047f199a9d86c921e0
paptil9 <113862880+paptil9@users.noreply.github.com>
2022-09-18 22:04:25 -0500

    Create top.svh

---
+++ Testbench/top.svh
@@ -0,0 +1,15 @@
+
+  `include "transaction.sv"
+
+  `include "interface.sv"
+
+  `include "d_config.sv"
+
+  `include"generator.sv"
+  `include"driver.sv"
+  `include"pmonitor.sv"
+  `include"scoreboard.sv"
+  `include "environment.sv"
+  `include "test.sv"
+  `include "checkerflag.sv"
+
